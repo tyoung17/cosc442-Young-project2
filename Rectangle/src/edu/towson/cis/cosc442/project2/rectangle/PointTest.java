e733a0bbadb7d1a00cf361eb8ebc962cddf4bd1d
Tyler Young <tyoung17@students.towson.edu>
2017-02-26 22:57:56 -0500

    Added Javadoc to PointTest class in Rectangle Project

--- Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/PointTest.java
+++ Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/PointTest.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package edu.towson.cis.cosc442.project2.rectangle;
 
 import static org.junit.Assert.*;
@@ -6,26 +9,47 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PointTest.
+ */
 public class PointTest {
+	
+	/** The p 2. */
 	Point p1, p2;
 	
+	/**
+	 * Sets the up.
+	 *
+	 * @throws Exception the exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		p1 = new Point(4.0, 6.0);
 		p2 = new Point(2.0, 2.0);
 	}
 	
+	/**
+	 * Test get X.
+	 */
 	@Test
 	public void testGetX() {
 		assertEquals(4.0, p1.getX(), 0.001);
 		assertEquals(2.0, p2.getX(), 0.001);
 	}
+	
+	/**
+	 * Test get Y.
+	 */
 	@Test
 	public void testGetY() {
 		assertEquals(6.0, p1.getY(), 0.001);
 		assertEquals(2.0, p2.getY(), 0.001);
 	}
 	
+	/**
+	 * Test set X.
+	 */
 	@Test
 	public void testSetX() {
 		p1.setX(4.0);
@@ -35,6 +59,9 @@ public class PointTest {
 		assertEquals(12.0, p2.getX(), 0.001);
 	}
 	
+	/**
+	 * Test set Y.
+	 */
 	@Test
 	public void testSetY() {
 		p1.setY(3.0);
@@ -44,6 +71,9 @@ public class PointTest {
 		assertEquals(7.0, p2.getY(), 0.001);
 	}
 	
+	/**
+	 * Tear down.
+	 */
 	@After
 	public void tearDown() {
 		p1 = null;
